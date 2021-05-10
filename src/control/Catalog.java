46975cbcc43121cb65caa7b8b5db26559e88f419
Anzalone7 <anzalone.domenico07@gmail.com>
2021-05-10 11:22:36 +0200

    test commit Anzalone

--- src/control/Catalog.java
+++ src/control/Catalog.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Catalog")
 public class Catalog extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	//prova_3
     /**
      * Default constructor. 
      */
